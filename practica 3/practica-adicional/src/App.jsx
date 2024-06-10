a14ccaceedc7844fc32f20200b52a8bb77db1077
Priscila Di Renzo <prisciladirenzo@gmail.com>
2024-06-10 18:33:27 -0300

    pratica-3-adicional

---
+++ practica 3/practica-adicional/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TableForm from './components/tableForm/TableForm';
+
+function App() {
+
+
+  return (
+    <>
+      <TableForm/>
+    </>
+  )
+}
+
+export default App
